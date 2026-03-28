37532875633406af2f5cde0fad6cd5ee744a2c57
anas <a.qasem@pixilapps.com>
2026-03-28 21:47:15 +0300

    hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/hello-world')
+  getHelloWorld(): string {
+    return this.appService.getHelloworld();
+  }
+
+  @Post('/hello')
+  postHello(@Query('name') name): string {
+    return this.appService.sayWelcomeToTheUser(name);
+  }
+
+  @Post('/helloParams/:name')
+  postHelloParams(@Param() prams): string {
+    console.log(prams);
+    return this.appService.sayWelcomeToTheUser("");
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    return 'Hello WOW!';
+  }
+
+  getHelloworld(): string {
     return 'Hello World!';
+  }
+
+  sayWelcomeToTheUser(name: string): string {
+    return `Welcome to the NestJS world, ${name}!`;
   }
 }
